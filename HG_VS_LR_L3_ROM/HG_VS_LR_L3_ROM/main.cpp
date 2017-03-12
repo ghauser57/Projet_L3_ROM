70db131507cd1d2208cc0384038524891a2728cd
KRATHEON <sullimoi@hotmail.fr>
2017-03-12 17:19:19 +0100

    debut de lecture

--- HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/main.cpp
+++ HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/main.cpp
@@ -1,52 +1,12 @@
 #pragma once
-//#include "LectureGPR.cpp"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <stdlib.h> 
-
-using namespace std;
+#include "LectureGPR.h"
 
 int main()
 {
-	ifstream fichier("gpr_files/data_VRPTW_10_3_2_4.gpr");
-
-	if (fichier)
-	{
-		string ligne; //Une variable pour stocker les lignes lues
-		int n;
-		int position;
-		//int m;
-
-		while (getline(fichier, ligne)) //Tant qu'on n'est pas à la fin, on lit
-		{
-		cout << ligne << endl;
-
-		//fichier.seekg(0, ios::end);
-		//fichier >> n;
-		//fichier.seekg(8, ios::cur);
-		//fichier >> m;
-		//position = fichier.tellg();
-		//cout << n << endl;
-
-		//cout << position << endl;
+	//GrpToGraphe("gpr_files/data_VRPTW_10.gpr");
 
 
-		//getline(fichier, ligne);
-		//getline(fichier, ligne);
-		//cout << ligne << endl;
-		//position = fichier.tellg();
-		//cout << position << endl;
-		}
-	}
-	else
-	{
-		cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
-	}
-
-
-
-	system("PAUSE");
+	//system("PAUSE");
 
 	return 0;
 }
