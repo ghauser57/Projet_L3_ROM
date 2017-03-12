3de7027f7e2f12657270acea77b8f0027536b8c9
KRATHEON <sullimoi@hotmail.fr>
2017-03-12 19:14:28 +0100

    lecture du fichier grp fini

--- HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/LectureGPR.cpp
+++ HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/LectureGPR.cpp
@@ -1,105 +1,182 @@
 #ifdef _MSC_VER
-
 #define _CRT_SECURE_NO_WARNINGS
-
 #endif
 
-
-
 #pragma once
-
 #include "Erreur.h"
-
 #include <fstream>
-
 #include <stdlib.h> 
+#include <vector>
 
+//trim d'un char *
+char *trimChar(char *str)
+{
+	char *end;
 
+	// Trim leading space
+	while (isspace((unsigned char)*str)) str++;
 
+	if (*str == 0)  // All spaces?
+		return str;
+
+	// Trim trailing space
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end)) end--;
+
+	// Write new null terminator
+	*(end + 1) = 0;
+
+	return str;
+}
 
 
 //template <S,T>
-
-void GrpToGraphe(const string & fileName)
-
+void GprToGraphe()
 {
-
-	ifstream fichier(fileName);
-
-
+	ifstream fichier("gpr_files/data_VRPTW_10.gpr");
 
 	if (fichier)
-
 	{
-
 		string line;
-
 		string str;
-
-		int nbRessources;
-
-
+		string nbRessources;
+		char msg[500];
 
 		getline(fichier, line);
 
+		//stocker le n° nb ressources
+		fichier >> str;
+		fichier >> nbRessources;
+		fichier >> str;
 
+		//mettre le curseur sur le premier des sections sommets
+		getline(fichier, line);
+		getline(fichier, line);
 
-		int N = 0, M = 0;
+		cout << "==================" << endl;
+		cout << "SECTIONS SOMMETS" << endl;
+		cout << "==================" << endl;
 
+		// parcours des sections sommets et stockage des valeurs de chaque sommets
+		while (line != "sources"){
 
+			strcpy(msg, line.c_str());
+			vector<char *> sommets;
+			char * pch = strtok(msg, " ");
 
-		sscanf(line.c_str(), "# Graphe N = %d ; M = %d", &N, &M);
-
-
-
-		if (N != 0 && M != 0){
-
-			// Valeur récupérée avec succès !
-
-
-
-			cout << "N = " << N << " et M = " << M << endl;
-
-
-
-			fichier >> str;
-
-			fichier >> nbRessources;
-
-			fichier >> str;
-
-
-
-			for (int i = 0; i < N; i++){
-
-
-
+			while (pch != NULL)
+			{
+				sommets.push_back(trimChar(pch));
+				pch = strtok(NULL, " ");
 			}
 
-		}
+			for (vector<char *>::iterator iter = sommets.begin(); iter != sommets.end(); iter++)
+				cout << *iter << endl;
 
-		else{
-
-			// Erreur (La chaîne ne corresponds pas au format)
-
-
-
-			cout << "Erreur de valeur du M " << endl;
+			getline(fichier, line);
 
 		}
 
+		cout << "==================" << endl;
+		cout << " SOURCES" << endl;
+		cout << "==================" << endl;
+		getline(fichier, line);
 
+		// parcours des secgions sommets et stockage des valeurs de chaque sources
+		while (line != "puits"){
+
+			strcpy(msg, line.c_str());
+			vector<char *> sources;
+			char * pch = strtok(msg, " ");
+
+			while (pch != NULL)
+			{
+				sources.push_back(trimChar(pch));
+				pch = strtok(NULL, " ");
+			}
+
+			for (vector<char *>::iterator iter = sources.begin(); iter != sources.end(); iter++)
+				cout << *iter << endl;
+
+			getline(fichier, line);
+		}
+
+		cout << "==================" << endl;
+		cout << " PUITS" << endl;
+		cout << "==================" << endl;
+		getline(fichier, line);
+
+		// parcours des secgions sommets et stockage des valeurs de chaque puits
+		while (line != "sectionArcs"){
+
+			strcpy(msg, line.c_str());
+			vector<char *> puits;
+			char * pch = strtok(msg, " ");
+
+			while (pch != NULL)
+			{
+				puits.push_back(trimChar(pch));
+				pch = strtok(NULL, " ");
+			}
+
+			for (vector<char *>::iterator iter = puits.begin(); iter != puits.end(); iter++)
+				cout << *iter << endl;
+
+			getline(fichier, line);
+
+		}
+
+		cout << "==================" << endl;
+		cout << " ARCS" << endl;
+		cout << "==================" << endl;
+		getline(fichier, line);
+
+		// parcours des secgions sommets et stockage des valeurs de chaque puits
+		while (line != "sectionGraphes"){
+
+			strcpy(msg, line.c_str());
+			vector<char *> arcs;
+			char * pch = strtok(msg, " ");
+
+			while (pch != NULL)
+			{
+				arcs.push_back(trimChar(pch));
+				pch = strtok(NULL, " ");
+			}
+
+			for (vector<char *>::iterator iter = arcs.begin(); iter != arcs.end(); iter++)
+				cout << *iter << endl;
+
+			getline(fichier, line);
+
+		}
+
+		cout << "==================" << endl;
+		cout << " GRAPHE" << endl;
+		cout << "==================" << endl;
+
+		// parcours des secgions sommets et stockage des valeurs de chaque puits
+		while (getline(fichier, line)){
+
+			strcpy(msg, line.c_str());
+			vector<char *> arcs;
+			char * pch = strtok(msg, " ");
+
+			while (pch != NULL)
+			{
+				arcs.push_back(trimChar(pch));
+				pch = strtok(NULL, " ");
+			}
+
+			for (vector<char *>::iterator iter = arcs.begin(); iter != arcs.end(); iter++)
+				cout << *iter << endl;
+
+		}
 
 	}
-
 	else
-
 	{
-
 		cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
-
 	}
-
-
 
 }
--- HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/main.cpp
+++ HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/main.cpp
@@ -1,99 +1,11 @@
-#ifdef _MSC_VER
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
 #pragma once
-#include "Erreur.h"
-#include <fstream>
-#include <stdlib.h> 
-#include <vector>
-//#include "LectureGPR.cpp"
+#include "LectureGPR.cpp"
 
-//trim d'un char *
-char *trimChar(char *str)
-{
-	char *end;
-
-	// Trim leading space
-	while (isspace((unsigned char)*str)) str++;
-
-	if (*str == 0)  // All spaces?
-		return str;
-
-	// Trim trailing space
-	end = str + strlen(str) - 1;
-	while (end > str && isspace((unsigned char)*end)) end--;
-
-	// Write new null terminator
-	*(end + 1) = 0;
-
-	return str;
-}
 
 
 int main()
 {
-	ifstream fichier("gpr_files/data_VRPTW_10.gpr");
-
-	if (fichier)
-	{
-		string line;
-		string str;
-		int nbRessources;
-		char msg[500];
-
-		getline(fichier, line);
-
-		int N = 0, M = 0;
-
-		sscanf(line.c_str(), "# Graphe N = %d ; M = %d", &N, &M);
-
-		if (N != 0 && M != 0){
-			// Valeur récupérée avec succès !
-
-			cout << "N = " << N << " et M = " << M << endl;
-
-			//stocker le n° nb ressources
-			fichier >> str;
-			fichier >> nbRessources;
-			fichier >> str;
-
-			//mettre le curseur sur le premier des sections sommets
-			getline(fichier, line);
-			getline(fichier, line);
-
-			// parcours des secgions sommets et stockage des valeurs de chaque sommets
-			while (line !="sources"){
-
-				strcpy(msg, line.c_str());
-				vector<char *> message;
-				char * pch = strtok(msg, " ");
-
-				while (pch != NULL)
-				{
-					message.push_back(trimChar(pch));
-					pch = strtok(NULL, " ");
-				}
-
-				for (vector<char *>::iterator iter = message.begin(); iter != message.end(); iter++)
-					cout << *iter << endl;
-
-				getline(fichier, line);
-		
-			}
-
-			
-		}
-		else{
-			// Erreur (La chaîne ne corresponds pas au format)
-			cout << "Erreur de valeur du M " << endl;
-		}
-
-	}
-	else
-	{
-		cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
-	}
+	GprToGraphe();
 
 	system("PAUSE");
 
