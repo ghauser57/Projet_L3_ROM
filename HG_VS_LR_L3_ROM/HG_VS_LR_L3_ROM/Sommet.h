c4bbb5f02e721252fa484723578b0a1168dcd719
ghauser57 <g.hauser96@gmail.com>
2017-03-18 18:18:52 +0100

    Ajout pere dans Sommet

--- HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/Sommet.h
+++ HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/Sommet.h
@@ -10,14 +10,15 @@ public:
 	int dPlus, dMoins;
 	string nom;
 	T bornInf, bornSup;
+	int pere;
 	int poids;
 	int n, num, ncomp;
 	int prefixe, suffixe;
 	bool marquage;
 	
-	Sommet(const string & nom, const T & bornInf, const T & bornSup) :dPlus(0), dMoins(0), nom(nom), bornInf(bornInf), bornSup(bornSup), poids(0), n(0), num(0), ncomp(0), marquage(false), prefixe(0), suffixe(0){}
-	Sommet(const string & nom) :dPlus(0), dMoins(0), bornInf(0), bornSup(0), poids(0), n(0), num(0), ncomp(0), marquage(false), prefixe(0), suffixe(0){}
-	Sommet(const Sommet<T> & s) :dPlus(0), dMoins(0), nom(s.nom), bornInf(s.bornInf), bornSup(s.bornSup), poids(s.poids), n(s.n), num(s.num), ncomp(s.ncomp), marquage(s.marquage), prefixe(s.prefixe), suffixe(s.suffixe){}
+	Sommet(const string & nom, const T & bornInf, const T & bornSup) :dPlus(0), dMoins(0), nom(nom), bornInf(bornInf), bornSup(bornSup), pere(-1), poids(0), n(0), num(0), ncomp(0), marquage(false), prefixe(0), suffixe(0){}
+	Sommet(const string & nom) :dPlus(0), dMoins(0), bornInf(0), bornSup(0), pere(-1), poids(0), n(0), num(0), ncomp(0), marquage(false), prefixe(0), suffixe(0){}
+	Sommet(const Sommet<T> & s) :dPlus(0), dMoins(0), nom(s.nom), bornInf(s.bornInf), bornSup(s.bornSup), pere(s.pere), poids(s.poids), n(s.n), num(s.num), ncomp(s.ncomp), marquage(s.marquage), prefixe(s.prefixe), suffixe(s.suffixe){}
 	bool operator == (const Sommet<T> & s) const;
 	bool operator != (const Sommet<T> & s) const;
 	operator string () const;
