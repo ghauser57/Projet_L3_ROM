56f6949561f0c8f52eb5ee1ba42e991faa0b9c36
KRATHEON <sullimoi@hotmail.fr>
2017-03-18 18:29:45 +0100

    ajout des debut de Ford

---
+++ HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/Ford.h
@@ -0,0 +1,17 @@
+#include "Graphe.h"
+
+template <class S, class T>
+void Graphe<S, T>::Ford(){
+
+	source->poids = 0;
+
+	for(int unsigned i = 1; i = lSommets.size(); i++){
+		lSommets.at(i)->poids = exp(99);
+	}
+
+	vector< pair< Sommet<T> *, Arete<S, T>* > > adjP = adjacencesPlus(lSommets.at(i));
+	for (int unsigned j = 0; j < adjP.size(); j++){
+
+	}
+	
+}
