6e3ca4d87aa2be6e276b2e1f683804a1162cd19c
KRATHEON <sullimoi@hotmail.fr>
2017-03-18 19:16:47 +0100

    Ford fini

--- HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/Ford.h
+++ HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/Ford.h
@@ -9,9 +9,17 @@ void Graphe<S, T>::Ford(){
 		lSommets.at(i)->poids = exp(99);
 	}
 
-	vector< pair< Sommet<T> *, Arete<S, T>* > > adjP = adjacencesPlus(lSommets.at(i));
-	for (int unsigned j = 0; j < adjP.size(); j++){
 
+	for (int unsigned i = 0; i < lSommets.size(); i++){
+
+		vector< pair< Sommet<T> *, Arete<S, T>* > > adjP = adjacencesPlus(lSommets.at(i));
+		for (int unsigned j = 0; j < adjP.size(); j++){
+
+			if (adjP.at(j).first->poids > (adjP.at(j).first->poids + adjP.at(j).second->cout)){
+
+				adjP.at(j).first->poids = adjP.at(j).first->poids + adjP.at(j).second->cout;
+				adjP.at(j).first->pere = i;
+			}
+		}
 	}
-	
 }
