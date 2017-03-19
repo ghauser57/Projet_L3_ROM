dbca8da065de7135a948dac6c75722c1ed93f38b
KRATHEON <sullimoi@hotmail.fr>
2017-03-19 16:22:09 +0100

    Ford qui marche

--- HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/Ford.h
+++ HG_VS_LR_L3_ROM/HG_VS_LR_L3_ROM/Ford.h
@@ -1,24 +1,24 @@
 #include "Graphe.h"
 
 template <class S, class T>
-void Ford(){
+void ford(){
 
 	source->poids = 0;
 
-	for(int unsigned i = 0; i = lSommets.size(); i++){
-		lSommets.at(i)->poids = exp(99);
+	for (int unsigned i = 1; i < lSommets.size(); i++){
+		lSommets.at(i)->poids = 9999999;
 	}
-
 
 	for (int unsigned k = 0; k < lSommets.size(); k++){
 
 		vector< pair< Sommet<T> *, Arete<S, T>* > > adjP = adjacencesPlus(lSommets.at(k));
+
 		for (int unsigned j = 0; j < adjP.size(); j++){
 
-			if (adjP.at(j).first->poids  >(adjP.at(j).first->poids + adjP.at(j).second->cout)){
+			if (poidsJ  >(poidsPere + poidsArc)){
 
-				adjP.at(j).first->poids = adjP.at(j).first->poids + adjP.at(j).second->cout;
-				adjP.at(j).first->pere = i;
+				adjP.at(j).first->poids = lSommets.at(k)->poids + adjP.at(j).second->cout;
+				adjP.at(j).first->pere = k;
 			}
 		}
 	}
